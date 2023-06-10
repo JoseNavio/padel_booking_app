42e537edff5eae530e07541bb426447b14523cf4
Jose Navío <josenavio747@gmail.com>
2023-06-10 17:56:48 +0200

    _NEW_ Initial commit

---
+++ app/src/main/java/me/josena/padel/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.josena.padel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
